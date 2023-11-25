12855739f24abde517a2ba2209bdc8d23ae25a83
danielgeorge1234 <101335387+danielgeorge1234@users.noreply.github.com>
2023-11-25 13:18:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+using CRUD.BLL.Interfaces;
+using CRUD.BLL.Repository;
+using CRUD.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<AppGroup01DbContext>(options =>
+			{
+
+				options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+			});
+
+			builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+			builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
